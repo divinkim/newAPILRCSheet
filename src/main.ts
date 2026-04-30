0e8f6537942c227c558b69e42ec2dbfc9537ed6a
lorealdivin <doe99067@gmail.com>
2026-04-30 14:42:09 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import 'tsconfig-paths/register';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const isProd = process.env.NODE_ENV === 'production';
+
+  const app = isProd
+    ? await NestFactory.create(AppModule, {
+        httpsOptions: {
+          key: require('fs').readFileSync('/etc/letsencrypt/live/vps118934.serveur-vps.net/privkey.pem'),
+          cert: require('fs').readFileSync('/etc/letsencrypt/live/vps118934.serveur-vps.net/fullchain.pem'),
+        },
+      })
+    : await NestFactory.create(AppModule);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
